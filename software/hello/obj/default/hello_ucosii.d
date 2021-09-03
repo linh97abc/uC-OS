bfa0101ae9705649f7fcec0c838be9d5e4a4b2ab
linh97abc <linh97abc@gmail.com>
2021-09-03 14:05:16 +0700

    first commit

---
+++ software/hello/obj/default/hello_ucosii.d
@@ -0,0 +1,35 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+ ../hello_bsp//HAL/inc/includes.h ../hello_bsp//HAL/inc/os_cpu.h \
+ ../hello_bsp//HAL/inc/sys/alt_irq.h ../hello_bsp//HAL/inc/nios2.h \
+ ../hello_bsp//HAL/inc/alt_types.h ../hello_bsp/system.h \
+ ../hello_bsp/linker.h ../hello_bsp//UCOSII/inc/os_cfg.h \
+ ../hello_bsp//HAL/inc/sys/alt_alarm.h \
+ ../hello_bsp//HAL/inc/sys/alt_llist.h \
+ ../hello_bsp//HAL/inc/priv/alt_alarm.h \
+ ../hello_bsp//UCOSII/inc/ucos_ii.h ../hello_bsp//HAL/inc/os_cpu.h
+
+../hello_bsp//HAL/inc/includes.h:
+
+../hello_bsp//HAL/inc/os_cpu.h:
+
+../hello_bsp//HAL/inc/sys/alt_irq.h:
+
+../hello_bsp//HAL/inc/nios2.h:
+
+../hello_bsp//HAL/inc/alt_types.h:
+
+../hello_bsp/system.h:
+
+../hello_bsp/linker.h:
+
+../hello_bsp//UCOSII/inc/os_cfg.h:
+
+../hello_bsp//HAL/inc/sys/alt_alarm.h:
+
+../hello_bsp//HAL/inc/sys/alt_llist.h:
+
+../hello_bsp//HAL/inc/priv/alt_alarm.h:
+
+../hello_bsp//UCOSII/inc/ucos_ii.h:
+
+../hello_bsp//HAL/inc/os_cpu.h:
